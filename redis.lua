8530d8f26ef16e66ab26b56d908c4301bee99b9c
Leo P <junk@slact.net>
2010-07-09 13:12:16 -0400

    Minor correction to response.multibulk to make it work.

--- redis.lua
+++ redis.lua
@@ -129,7 +129,7 @@ function response.multibulk(client, data)
         local list = {}
         if list_count > 0 then 
             for i = 1, list_count do
-                table.insert(list, i, response.bulk(client, network.read(client)))
+	       table.insert(list, i, response.read(client))
             end
         end
         return list
