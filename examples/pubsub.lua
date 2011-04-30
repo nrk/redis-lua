99716bcc8a90dd8bc038d6758c2df1c636f26bef
Daniele Alessandri <suppakilla@gmail.com>
2011-04-30 15:43:59 +0200

    Add an example of a pub/sub consumer loop.

---
+++ examples/pubsub.lua
@@ -0,0 +1,36 @@
+package.path = package.path .. ";../src/?.lua"
+
+require 'redis'
+
+local params = {
+    --host = '127.0.0.1',
+    host = '192.168.1.31',
+    port = 6379,
+}
+
+local redis = Redis.connect(params)
+redis:select(15) -- for testing purposes
+
+local channels = { 'control_channel', 'notifications' }
+
+-- Start processing the pubsup messages. Open a terminal and use redis-cli
+-- to push messages to the channels. Examples:
+--   ./redis-cli PUBLISH notifications "this is a test"
+--   ./redis-cli PUBLISH control_channel quit_loop
+
+for msg, abort in redis:pubsub({ subscribe = channels }) do
+    if msg.kind == 'subscribe' then
+        print('Subscribed to channel '..msg.channel)
+    elseif msg.kind == 'message' then
+        if msg.channel == 'control_channel' then
+            if msg.payload == 'quit_loop' then
+                print('Aborting pubsub loop...')
+                abort()
+            else
+                print('Received an unrecognized command: '..msg.payload)
+            end
+        else
+            print('Received the following message from '..msg.channel.."\n  "..msg.payload.."\n")
+        end
+    end
+end
