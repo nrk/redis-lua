866727636feca5b92cf5442237a262b0a6be72ba
Pierre Chapuis <catwell-github@catwell.info>
2013-08-19 16:25:51 +0200

    Return score as a number when parsing response from ZINCRBY.

--- src/redis.lua
+++ src/redis.lua
@@ -928,7 +928,11 @@ redis.commands = {
 
     -- commands operating on sorted sets
     zadd             = command('ZADD'),
-    zincrby          = command('ZINCRBY'),
+    zincrby          = command('ZINCRBY', {
+        response = function(reply, command, ...)
+            return tonumber(reply)
+        end,
+    }),
     zrem             = command('ZREM'),
     zrange           = command('ZRANGE', {
         request  = zset_range_request,
